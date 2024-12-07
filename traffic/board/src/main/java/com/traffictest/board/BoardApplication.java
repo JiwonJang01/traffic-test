e508effcccb60cc4f5376e072865c00497787c80
rhino.k <ewb.doohyun@gmail.com>
2024-12-07 13:34:33 +0900

    [001] Settings ASIS

--- traffic/board/src/main/java/com/traffictest/board/BoardApplication.java
+++ traffic/board/src/main/java/com/traffictest/board/BoardApplication.java
@@ -2,7 +2,9 @@ package com.traffictest.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(com.traffictest.common.config.JpaConfiguration.class)
 @SpringBootApplication
 public class BoardApplication {
     public static void main(String[] args) {
--- traffic/statistics/src/main/java/com/traffictest/statistics/StatisticsApplication.java
+++ traffic/statistics/src/main/java/com/traffictest/statistics/StatisticsApplication.java
@@ -2,7 +2,9 @@ package com.traffictest.statistics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(com.traffictest.common.config.JpaConfiguration.class)
 @SpringBootApplication
 public class StatisticsApplication {
     public static void main(String[] args) {
