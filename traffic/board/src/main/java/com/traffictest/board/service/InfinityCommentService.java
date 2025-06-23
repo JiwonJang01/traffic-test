e8ce75e1b86b20d3fcea97e21c9e487b9a37c2b5
JiwonJang01 <fx567849@gmail.com>
2025-06-23 21:43:12 +0900

    feat: comment depth

--- traffic/board/src/main/java/com/traffictest/board/service/InfinityCommentService.java
+++ traffic/board/src/main/java/com/traffictest/board/service/InfinityCommentService.java
@@ -22,6 +22,7 @@ public class InfinityCommentService {
         return infinityCommentRepository.findCommentHierarchy();
     }
 
+    // suppose 1000;
     public void init(int count) {
         int counter = 0;
         List<User> users = new ArrayList<>();
@@ -33,10 +34,8 @@ public class InfinityCommentService {
         userRepository.saveAll(users);
         // 5단계 댓글 생성
         // need fix
-        // 댓글 대댓
-        //
         while (counter < count) {
-            // make 100 depth;
+            // make 20 depth;
             List<InfinityComment> infinityComments = new ArrayList<>();
             // 첫 comment
             InfinityComment comment = InfinityComment.builder()
@@ -56,7 +55,8 @@ public class InfinityCommentService {
             infinityComments.clear();
         }
     }
-    public void enhancedInit(int count){
+
+    public void enhancedInit(int count) {
         int counter = 0;
         List<User> users = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
@@ -68,26 +68,26 @@ public class InfinityCommentService {
 
         List<InfinityComment> buffer = new ArrayList<>();
         InfinityComment comment = InfinityComment.builder()
-                .content("comment" + counter)
+                .content("commnet =" + counter)
                 .writer(users.get(0))
                 .build();
         counter++;
         buffer.add(comment);
         List<InfinityComment> parents = new ArrayList<>();
-        List<InfinityComment> children = new ArrayList<>();
+        List<InfinityComment> childrens = new ArrayList<>();
         parents.add(comment);
         while (counter < count) {
-            for(InfinityComment parent : parents) {
-                for(int i=0; i<parents.size() * 2; i++) {
+            for(InfinityComment parent : parents){
+                for(int i=0;i<parents.size() * 2;i++){
                     comment = InfinityComment.builder()
-                            .content("comment" + counter)
+                            .content("commnet =" + counter)
                             .parent(parent)
                             .writer(users.get(counter++ % 10))
                             .build();
                     childrens.add(comment);
                     buffer.add(comment);
 
-                    if(buffer.size() == 1000){
+                    if( buffer.size() == 1000){
                         infinityCommentRepository.saveAll(buffer);
                         buffer.clear();
                     }
@@ -98,8 +98,8 @@ public class InfinityCommentService {
                 }
             }
             parents.clear();
-            parents = children;
-            children = new ArrayList<>();
+            parents = childrens;
+            childrens = new ArrayList<>();
         }
     }
 }
